523db7d1edc38e670e55192338b208b71ec51564
Abhishek Pandit <policyglot@users.noreply.github.com>
2020-07-20 22:00:57 -0500

    Add basic 3-file structure
    type: style
    Included the process_data, run & train_classifier files. They will form the mainstay of the rest of the data pipelines and web app

---
+++ process_data.py
@@ -0,0 +1,7 @@
+
+# import libraries
+import pandas as pd
+# load messages dataset
+messages = pd.read_csv('messages.csv')
+# load categories dataset
+categories = pd.read_csv('categories.csv')
---
+++ run.py
@@ -0,0 +1,3 @@
+"""
+This code helps run the web app
+"""
