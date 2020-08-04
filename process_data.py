c518138e362c5387c7215d30ae07ceb6ef966294
Abhishek Pandit <policyglot@users.noreply.github.com>
2020-08-03 23:40:04 -0500

    Split categories column in 36 sub-categories
    type:feat
    body: Used lambda functions to first split the composite string of classifications under categories into categories first (as column headings) and then binary values

--- process_data.py
+++ process_data.py
@@ -5,3 +5,24 @@ import pandas as pd
 messages = pd.read_csv('messages.csv')
 # load categories dataset
 categories = pd.read_csv('categories.csv')
+
+# merge datasets
+df = pd.merge(right=messages, left=categories, on="id")
+# create a dataframe of the 36 individual category columns
+categories =  df['categories'].str.split(';', expand=True)
+row = categories.iloc[0]
+category_colnames = row.apply(lambda x: x[:-2])
+categories.columns = category_colnames
+
+for column in categories:
+    # set each value to be the last character of the string
+    categories[column] = categories[column].apply(lambda x: x[-1])
+    
+    # convert column from string to numeric
+    categories[column] = categories[column].astype(int)
+
+# drop the original categories column from `df`
+df.drop('categories', axis=1, inplace=True)
+df.head()
+
+df = pd.concat([df,categories], axis=1)
