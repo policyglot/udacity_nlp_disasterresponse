559b27b96d68c9199b3443669106bad001538def
Abhishek Pandit <policyglot@users.noreply.github.com>
2020-08-20 20:32:03 -0500

    refactor: convert code to functions for command line

--- process_data.py
+++ process_data.py
@@ -1,35 +1,96 @@
 
-# import libraries
+import sys
 import pandas as pd
 from sqlalchemy import create_engine
-# load messages dataset
-messages = pd.read_csv('messages.csv')
-# load categories dataset
-categories = pd.read_csv('categories.csv')
 
-# merge datasets
-df = pd.merge(right=messages, left=categories, on="id")
-# create a dataframe of the 36 individual category columns
-categories =  df['categories'].str.split(';', expand=True)
-row = categories.iloc[0]
-category_colnames = row.apply(lambda x: x[:-2])
-categories.columns = category_colnames
 
-for column in categories:
-    # set each value to be the last character of the string
-    categories[column] = categories[column].apply(lambda x: x[-1])
+def load_data(messages_filepath, categories_filepath):
+    """
+    Merges the two csv files provided using the 'id' column
     
-    # convert column from string to numeric
-    categories[column] = categories[column].astype(int)
+    Parameters:
+    messages_filepath- string 
+    categories-filepath- string
+    
+    Returns:
+    df- data frame 
+    """
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = pd.merge(right=messages, left=categories, on="id")
+    return df   
 
-# drop the original categories column from `df`
-df.drop('categories', axis=1, inplace=True)
-df.head()
+def clean_data(df):
+    """
+    Performs two cleaning tasks- splitting of the categories
+    column, renaming the columns and dropping duplicates
+    
+    Arguments: 
+    df- data frame object
+    
+    Returns: df
+    df- data frame object
+    """
+    
+    categories =  df['categories'].str.split(';', expand=True)
+    row = categories.iloc[0]
+    category_colnames = row.apply(lambda x: x[:-2])
+    categories.columns = category_colnames
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].apply(lambda x: x[-1])   
+        # convert column from string to numeric
+        categories[column] = categories[column].astype(int)
 
-df = pd.concat([df,categories], axis=1)
+    # drop the original categories column from `df`
+    df.drop('categories', axis=1, inplace=True)
+    df = pd.concat([df,categories], axis=1)
+    # drop duplicates
+    df = df.drop_duplicates()
+    return df
 
-# drop duplicates
-df = df.drop_duplicates()
-# create sql database
-engine = create_engine('sqlite:///disaster_response.db')
-df.to_sql('response_type', engine, index=False)
+def save_data(df, database_filename):
+    """
+    Creates a database and adds the dataframe as one of its
+    tables
+    
+    Parameters:
+    df- data frame
+    database_filename- string- address of database
+    
+    Returns:
+    None
+    """
+    engine = create_engine(database_filename)
+    df.to_sql(df, engine, index=False)
+    return 
+      
+
+def main():
+    if len(sys.argv) == 4:
+
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        df = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = clean_data(df)
+        
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df, database_filepath)
+        
+        print('Cleaned data saved to database!')
+    
+    else:
+        print('Please provide the filepaths of the messages and categories '\
+              'datasets as the first and second argument respectively, as '\
+              'well as the filepath of the database to save the cleaned data '\
+              'to as the third argument. \n\nExample: python process_data.py '\
+              'disaster_messages.csv disaster_categories.csv '\
+              'DisasterResponse.db')
+
+
+if __name__ == '__main__':
+    main()
