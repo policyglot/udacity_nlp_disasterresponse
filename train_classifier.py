8efc3f30fe9ea1818e0ff8038ceed148b1497de4
Abhishek Pandit <policyglot@users.noreply.github.com>
2020-09-20 23:51:26 -0500

    Include Vectorizer and TF-IDF imports
    feat: set up functions for more thorough cleaning with TF-IDF as ultimate features
    
    The tf-idf function needs to be completed so that the final input for the classifier is thet
    tf-idf array

--- train_classifier.py
+++ train_classifier.py
@@ -3,10 +3,25 @@ import pandas as pd
 import re
 from sqlalchemy import create_engine
 from scikitlearn.multioutput import MultipleOutputClassifier
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.metrics import classification_report
+# To import from the database
+from sqlalchemy import create_engine
+# To set up NLP pipelines
 from nltk import word_tokenizer
 # download necessary NLTK data
 import nltk
 nltk.download(['punkt', 'wordnet'])
+from nltk.corpus import stopwords
+from nltk.stem.wordnet import WordNetLemmatizer
+
+stop_words = stopwords.words("english")
+lemmatizer = WordNetLemmatizer()
+# initialize count vectorizer object
+vect = CountVectorizer(tokenizer=tokenize)
+
+
 
 #Extract data from SQL
 engine = create_engine('sqlite:///InsertDatabaseName.db')
@@ -18,8 +33,10 @@ df = pd.read_sql("SELECT * FROM population_data", engine)
 def tokenize(text):
     text = text.lower()
     text = re.sub(r'[^A-Za-x0-9]', ' ', text)
-    text = word_tokenizer(text)
-    return text
+    tokens = word_tokenizer(text)
+    tokens = [lemmatizer.lemmatize(word) for word in tokens if word not in stop_words]
+    return tokens
+
     
 X_train, X_test, y_train, y_test = test_train_split(X, y, test_size=0.33, random_seed=2020)
 
