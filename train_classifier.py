4050a9fcb24b8cd3ebd8007b526eebe60e6795e2
Abhishek Pandit <policyglot@users.noreply.github.com>
2020-12-12 13:35:02 -0600

    feat: complete all empty functions
    Adapted code from the Udacity notebook space to the function format.
    Will test code by running in the app and return for any issues that
    emerge.

--- train_classifier.py
+++ train_classifier.py
@@ -7,6 +7,13 @@ from nltk import word_tokenizer
 # download necessary NLTK data
 import nltk
 nltk.download(['punkt', 'wordnet'])
+from nltk.corpus import stopwords
+from nltk.stem.wordnet import WordNetLemmatizer
+
+stop_words = stopwords.words("english")
+lemmatizer = WordNetLemmatizer()
+
+from sklearn.feature_extraction.text import CountVectorizer
 
 #Extract data from SQL
 engine = create_engine('sqlite:///InsertDatabaseName.db')
@@ -18,8 +25,15 @@ df = pd.read_sql("SELECT * FROM population_data", engine)
 def tokenize(text):
     text = text.lower()
     text = re.sub(r'[^A-Za-x0-9]', ' ', text)
-    text = word_tokenizer(text)
-    return text
+    tokens = word_tokenizer(text)
+    tokens = [lemmatizer.lemmatize(word) for word in tokens if word not in stop_words]
+    return tokens
+
+def tf_idf(tokens):
+
+
+# initialize count vectorizer object
+vect = CountVectorizer(tokenizer=tokenize)
     
 X_train, X_test, y_train, y_test = test_train_split(X, y, test_size=0.33, random_seed=2020)
 
